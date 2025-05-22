c2f6b185c29c0a3db6a311c56d95e0633fc822a2
Rickem <urielmric@gmail.com>
2025-05-22 20:56:27 +0200

    Initial commit

---
+++ src/components/parallaxBackground.jsx
@@ -0,0 +1,28 @@
+import { motion, useScroll, useSpring, useTransform } from "motion/react";
+
+const ParallaxBackground = () => {
+  const { scrollYProgress } = useScroll();
+  const x = useSpring(scrollYProgress, { damping: 50 });
+  const mountain3Y = useTransform(x, [0, 0.5], ["0%", "70%"]);
+  const planetsX = useTransform(x, [0, 0.5], ["0%", "-20%"]);
+  const mountain2Y = useTransform(x, [0, 0.5], ["0%", "30%"]);
+  const mountain1Y = useTransform(x, [0, 0.5], ["0%", "0%"]);
+
+  return (
+    <section className="absolute inset-0 bg-black/40">
+      <div className="relative h-screen overflow-y-hidden">
+        {/* Background Sky */}
+        <div
+          className="absolute inset-0 w-full h-screen -z-50"
+          style={{
+            backgroundImage: "url(/assets/world.jpg)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+          }}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ParallaxBackground;
