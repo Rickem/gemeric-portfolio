47e94420517031128da5e32483979b5ce6ed9ea8
Rickem <urielmric@gmail.com>
2025-05-25 21:58:47 +0200

    Fixed about + parallax background

--- src/components/parallaxBackground.jsx
+++ src/components/parallaxBackground.jsx
@@ -15,9 +15,49 @@ const ParallaxBackground = () => {
         <div
           className="absolute inset-0 w-full h-screen -z-50"
           style={{
-            backgroundImage: "url(/assets/world.jpg)",
+            backgroundImage: "url(/assets/sky.jpg)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+          }}
+        />
+        {/* Mountain Layer 3 */}
+        <motion.div
+          className="absolute inset-0 -z-40"
+          style={{
+            backgroundImage: "url(/assets/mountain-3.png)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: mountain3Y,
+          }}
+        />
+        {/* Planets */}
+        <motion.div
+          className="absolute inset-0 -z-30"
+          style={{
+            backgroundImage: "url(/assets/planets.png)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            x: planetsX,
+          }}
+        />
+        {/* Mountain Layer 2 */}
+        <motion.div
+          className="absolute inset-0 -z-20"
+          style={{
+            backgroundImage: "url(/assets/mountain-2.png)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: mountain2Y,
+          }}
+        />
+        {/* Mountaine Layer 1 */}
+        <motion.div
+          className="absolute inset-0 -z-10"
+          style={{
+            backgroundImage: "url(/assets/mountain-1.png)",
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: mountain1Y,
           }}
         />
       </div>
